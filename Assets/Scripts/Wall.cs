b851aa17887a751e9f67d7c3a3f109bfc5b2e4f1
Sam Wu <sjwu@ualberta.ca>
2021-10-13 20:50:26 -0600

    setup wall script and damaged wall sprites

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -1,42 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 namespace Completed
 {
-	public class Wall : MonoBehaviour
-	{
-		public AudioClip chopSound1;				//1 of 2 audio clips that play when the wall is attacked by the player.
-		public AudioClip chopSound2;				//2 of 2 audio clips that play when the wall is attacked by the player.
-		public Sprite dmgSprite;					//Alternate sprite to display after Wall has been attacked by player.
-		public int hp = 3;							//hit points for the wall.
-		
-		
-		private SpriteRenderer spriteRenderer;		//Store a component reference to the attached SpriteRenderer.
-		
-		
-		void Awake ()
-		{
-			//Get a component reference to the SpriteRenderer.
-			spriteRenderer = GetComponent<SpriteRenderer> ();
-		}
-		
-		
-		//DamageWall is called when the player attacks a wall.
-		public void DamageWall (int loss)
-		{
-			//Call the RandomizeSfx function of SoundManager to play one of two chop sounds.
-			SoundManager.instance.RandomizeSfx (chopSound1, chopSound2);
-			
-			//Set spriteRenderer to the damaged wall sprite.
-			spriteRenderer.sprite = dmgSprite;
-			
-			//Subtract loss from hit point total.
-			hp -= loss;
-			
-			//If hit points are less than or equal to zero:
-			if(hp <= 0)
-				//Disable the gameObject.
-				gameObject.SetActive (false);
-		}
-	}
+    public class Wall : MonoBehaviour
+    {
+        public AudioClip playerAttackSound1;
+        public AudioClip playerAttackSound2;
+        public Sprite damagedImage;
+        public int hp = 3; // wall hitpoints/health points
+
+        private SpriteRenderer spriteRenderer;
+
+        void Awake()
+        {
+            //Get a component reference to the SpriteRenderer.
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        /// <summary>
+        /// Reduce wall health by loss. Disable wall if its hp is less than 1
+        /// </summary>
+        public void DamageWall(int loss)
+        {
+            SoundManager.instance.RandomizeSfx(playerAttackSound1, playerAttackSound2);
+            spriteRenderer.sprite = damagedImage;
+            hp -= loss;
+
+            if (hp <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
 }
